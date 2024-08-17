9569e1dd2195c31689efbd2bc51fa07912c6f140
Pearlish128624 <sunnine36@gmail.com>
2024-08-17 19:33:30 +0800

    loop_hw 更新

--- 3.Loop/loop_hw.py
+++ 3.Loop/loop_hw.py
@@ -9,24 +9,24 @@ def pattern_tree(level):
         print("The level of the tree must be odd.")
     else:
         for i in range (1,level+1): # 樹
-            spaces = " " * (level - i)  # 前置空格，使數字居中
+            spaces = " " * (level - i)  # 前面的空格，使數字居中
             numbers = str(i) * (i*2-1)  # 生成該行的數字，數量是2*i-1
-            print(spaces + numbers)  # 拼接空格和數字並打印
+            print(spaces + numbers)  
 
-        for i in range(level, 0, -1):  # 迴圈從 level 到 1, -1是步長，使數列倒序。
+        for i in range(level, 0, -1):  # 迴圈從 level 到 1, -1是步長，這樣數列才會倒序。
             spaces = " " * (level - 1)  # 每行需要的前置空格數量固定
             print(spaces + str(i))  # 樹幹
         
 def pattern_V(level):
 #level= int(input("Please input the level of the V: ")) 分開運行時寫的
     for i in range(level, 0, -1):
-        spacein= " "*(i*2-3)
-        spaceout=" "*(level-i)
+        spacein= " "*(i*2-3) #中間的空格
+        spaceout=" "*(level-i) #前面的空格
         print(spaceout+str(i)+spacein+str(i))
-        if i==2:
+        if i==2: #因為1只需要一個如果不停止會出現兩個1
             break
-    spacefinal=" "*(level-1)
-    print(spacefinal+str(1))
+    spacefinal=" "*(level-1) #1前面的空
+    print(spacefinal+str(1)) 
 
 def main():
     pattern_type = input("Please input the pattern（ladder, tree, V）: ").lower() #確保變成小寫==才有作用
