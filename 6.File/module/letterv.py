f6868fbd2f21bda7863710d41dfe4d53d3d7a550
Pearlish128624 <sunnine36@gmail.com>
2024-09-21 15:24:04 +0900

    update tree module_add wide

--- 6.File/module/letterv.py
+++ 6.File/module/letterv.py
@@ -1,10 +1,18 @@
-def pattern_V(level):
-#level= int(input("Please input the level of the V: ")) 分開運行時寫的
+def pattern_V(level, wide=False):
     for i in range(level, 0, -1):
-        spacein= " "*(i*2-3) #中間的空格
-        spaceout=" "*(level-i) #前面的空格
-        print(spaceout+str(i)+spacein+str(i))
-        if i==2: #因為1只需要一個如果不停止會出現兩個1
-            break
-    spacefinal=" "*(level-1) #1前面的空
-    print(spacefinal+str(1)) 
+        spaceout = " " * (level - i)  # 前面的空格
+        
+        if wide:
+            spacein = " " * (i * 2 - 1)  # 宽版中間的空格
+            print(spaceout + str(i) * 2 + spacein + str(i) * 2)
+        else:
+            spacein = " " * (i * 2 - 3)  # 普通版中間的空格
+            print(spaceout + str(i) + spacein + str(i))
+            if i == 2:  # 普通版在 i=2 时停止循环
+                break
+    
+    spacefinal = " " * (level - 1)  # 最后一行前面的空格
+    if wide:
+        print(spacefinal + str(1) * 2)  # 宽版最后一行打印两个1
+    else:
+        print(spacefinal + str(1))  # 普通版最后一行打印一个1
