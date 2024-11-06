80a626bcd02d0bca728437e956156e2130599931
poipoiyo <48159287+poipoiyo@users.noreply.github.com>
2024-11-06 22:13:29 +0800

    Feat: enhance input and something

--- 7.Practice1/flow.py
+++ 7.Practice1/flow.py
@@ -1,11 +1,13 @@
 # Game start
+import os
 import random
 # Read quesiton (1000 words)
 # Choose 1 of them, and get it's length
 # EX. cat -> _ _ _
 def ReadQuestion() -> tuple:
     # Read the file from the specified path
-    file_path = r"C:\Users\sunni\OneDrive1\OneDrive\Python\240701\python-practice\7.Practice1\source\1000.txt"
+    root_path = os.getcwd()
+    file_path = root_path + "\\source\\1000.txt"
     
     with open(file_path, 'r', encoding='utf-8') as file:
         words = file.readlines()  # Read all lines from the file
@@ -29,7 +31,7 @@ def Initialize() -> str:
     return player_name
 
 # Start guessing
-def StartGuessing(word: str, meaning: str, player_name: str) -> None:
+def StartGuessing(word: str, meaning: str) -> None:
     lives = 10
     score = 0
     
@@ -42,11 +44,18 @@ def StartGuessing(word: str, meaning: str, player_name: str) -> None:
         print(f"This word contains {word_length} letters.")
 
         while lives > 0:  # Round loop
-            guess = input("Enter a letter: ").lower()
+            
+            guess = "" # len = 0
+            while(len(guess) != 1):
+                guess = input("Enter a letter: ").lower()
+                if (guess == "exit") :
+                    return GameOver()
+                
             if guess in guessed_letters:
                 print("You already guessed that letter.")
+                print("Current guessed :", guessed_letters)
                 continue
-            guessed_letters.append(guess)
+            guessed_letters += guess
 
             if guess in word:
                 print("Correct guess!")
@@ -56,7 +65,6 @@ def StartGuessing(word: str, meaning: str, player_name: str) -> None:
 
             current_display = ''.join(letter if letter in guessed_letters else '_' for letter in word)
             print(f"Current word: {current_display}")
-
             if '_' not in current_display:
                 print(f"You finished the word: {word}")
                 print(f"Meaning: {meaning}")
@@ -87,5 +95,5 @@ if __name__ == "__main__":
     print("Welcome to the Hangman Game!")
     player_name = Initialize()
     word, meaning = ReadQuestion()  # Get the first word and its meaning
-    StartGuessing(word, meaning, player_name)
+    StartGuessing(word, meaning)
     
